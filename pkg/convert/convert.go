9a4394a58c55791f44dd96359b56bfa7ccf5a5ea
Gabe Koeb <gfkoeb@ncsu.edu>
2025-02-10 14:43:42 -0500

    add test and update for kebab

--- pkg/convert/convert.go
+++ pkg/convert/convert.go
@@ -9,13 +9,16 @@ func Convert(str string) string {
 
 	for i := 0; i < len(str); i++ {
 		var c rune = rune(str[i])
-		if unicode.IsUpper(c) {
+		// for kebab
+		if string(c) == "-" {
+			s += "_"
+		} else if unicode.IsUpper(c) { // for camel and pascal
 			if i != 0 {
 				s += "_" + string(unicode.ToLower(c))
 			} else {
 				s += string(unicode.ToLower(c))
 			}
-		} else {
+		} else { // add the char/rune
 			s += string(c)
 		}
 	}
--- pkg/convert/convert_test.go
+++ pkg/convert/convert_test.go
@@ -2,13 +2,34 @@ package convert
 
 import "testing"
 
+var snake string = "hello_world"
+
 func TestConvertCamel(t *testing.T) {
 	// test convert
-	snake := "hello_world"
 
-	camel_case := "helloWorld"
+	camelCase := "helloWorld"
 
-	if snake != Convert(camel_case) {
-		t.Errorf("Expected %s, got %s", snake, Convert(camel_case))
+	if snake != Convert(camelCase) {
+		t.Errorf("Expected %s, got %s", snake, Convert(camelCase))
+	}
+}
+
+func TestConvertPascal(t *testing.T) {
+	// test convert
+
+	pascalCase := "HelloWorld"
+
+	if snake != Convert(pascalCase) {
+		t.Errorf("Expected %s, got %s", snake, Convert(pascalCase))
+	}
+}
+
+func TestConvertKebab(t *testing.T) {
+	// test convert
+
+	kebabCase := "hello-world"
+
+	if snake != Convert(kebabCase) {
+		t.Errorf("Expected %s, got %s", snake, Convert(kebabCase))
 	}
 }
